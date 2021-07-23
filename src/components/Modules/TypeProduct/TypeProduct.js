8c81fe3bf93182b21ea709ebccb1bd4af53d8b3e
Ryez <52480059+lythiendu2507@users.noreply.github.com>
2021-07-23 12:21:28 +0700

    add slickslider -v2, fix and add picture basic for typeproduct

--- src/components/Modules/TypeProduct/TypeProduct.js
+++ src/components/Modules/TypeProduct/TypeProduct.js
@@ -3,12 +3,97 @@ import Link from 'next/link'
 
 export default function TypeProduct() {
   return (
-    <div className="tpm-form tp-form">
-      <Link href={`/products/`}>
-      <div>
-        <img/>
-        Đồ dùng học tập</div>
-      </Link>
+
+    <>
+    <p className="title-typeproduct"> Khám phá danh mục</p>
+    <div className="tpm-form tp-form ">
+      
+      <div className="tpm-form-deep">
+        <div className="tpm-form-row">
+          <Link href={`/products/`} className="">
+            <div className="tp span-tp-m item-tp-m ">
+            <div>
+            <img src="/img/typeproduct/sach.jpg"  className="img-tp-m img-tp"/>
+            </div>
+              
+
+              <span>Sách</span>
+              </div>
+          </Link>
+          <Link href={`/products/`}>
+          <div className="tp span-tp-m item-tp-m ">
+              <div>
+              <img src="/img/typeproduct/dodunghoctap.jpg" className="img-tp-m img-tp"/>
+              </div>
+              
+              <span>Đồ dùng học tập</span>
+              </div>
+          </Link>
+          <Link href={`/products/`}>
+          <div className="tp span-tp-m item-tp-m ">
+          <div>
+          <img src="/img/typeproduct/dodungsinhvien.jpg" className="img-tp-m img-tp"/>
+          </div>
+              
+              <span> Đồ dùng Sinh viên</span>
+             </div>
+          </Link>
+          <Link href={`/products/`}>
+          <div className="tp span-tp-m item-tp-m">
+          <div>
+          <img src="/img/typeproduct/dodungdientu.jpg" className="img-tp-m img-tp"/>
+          </div>
+              
+              <span>Đồ Dùng Điện Tử</span>
+              </div>
+          </Link>
+        </div>
+        <div className="tpm-form-row">
+          <Link href={`/products/`}>
+          <div className="tp span-tp-m item-tp-m">
+          <div>
+          <img src="/img/typeproduct/thietbigiadung.jpg" className="img-tp-m img-tp"/>
+          </div>
+              
+              <span>Thiết bị gia dụng</span>
+              
+            </div>
+          </Link>
+          <Link href={`/products/`}>
+          <div className="tp span-tp-m item-tp-m">
+          <div>
+          <img src="/img/typeproduct/dungcuthethao.jpg" className="img-tp-m img-tp"/>
+          </div>
+             
+              <span>Đồ thể thao</span>
+              
+            </div>
+          </Link>
+          <Link href={`/products/`}>
+          <div className="tp span-tp-m item-tp-m">
+          <div>
+          <img src="/img/typeproduct/dungcuthethao.jpg" className="img-tp-m img-tp"/>
+          </div>
+              
+              <span>Đồ thể thao</span>
+              
+            </div>
+          </Link>
+          <Link href={`/products/`}>
+          <div className="tp span-tp-m item-tp-m">
+          <div>
+          <img src="/img/typeproduct/dungcuthethao.jpg" className="img-tp-m img-tp"/>
+          </div>
+              
+              <span>Đồ thể thao</span>
+              
+            </div>
+          </Link>
+          
+          
+        </div>
+      </div>
     </div>
+    </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,11 @@ import { Card, Typography, Container } from '@material-ui/core';
 import styles from '../styles/Home.module.css'
 import MainLayout from '../components/Modules/MainLayout'
 import { ApolloClient, InMemoryCache, ApolloProvider, useApolloClient, defaultOptions, useQuery } from '@apollo/client'
-import Products from './products/index'
-import { GET_PRODUCTSTYPE } from '../graphql/getTypeProducts/getTypeProducts'
+
 import { GET_PRODUCTS } from '../graphql/getProducts/getProducts';
 import TypeProduct from '../components/Modules/TypeProduct/TypeProduct'
-import Product from './products/index'
-
+import SlickSlider from 'react-slick';
+import Slider from "react-slick";
 export default function Home({ products }) {
 
   const apolloClient = new ApolloClient({
@@ -19,57 +18,75 @@ export default function Home({ products }) {
     defaultOptions: defaultOptions
 
   })
-  const [producttype, setProducttype] = useState([]);
+  const [imgBanner, setImgBanner] = useState([
+    { img: "/img/meme1.jpg" },
+    { img: "/img/meme4.jpg" },
+    { img: "/img/meme5.jpg" },
+    { img: "/img/beaver1.jpg" },
+    { img: "/img/meme2.jpg" },
+    { img: "/img/meme3.jpg" }]);
  
-
   return (
 
     <ApolloProvider client={apolloClient}>
-
-      <MainLayout maxWidth="sm" className="products__products" />
       
+          <MainLayout  className="products__products all-all-m" />
 
-      <Container>
 
-        <div  className= "product__cards mb-3 my-4">
-        <TypeProduct/>
-        <div className="products container mobile-content my-3 my-sm-5">
-        <div className="d-flex flex-nowrap justify-content-between">
-          {/* <div className="products__sidebar pr-4 d-none d-sm-block">
+          
+          <Container>
+        <div className="product__cards mb-3 my-4 container-main">
+
+          <SlickSlider
+            arrows={false}
+            autoplay
+            dots ={true}
+
+            dotsClass="slick__dots bullet slick-dots"
+            className="align-items-center mb-0 slick-dotted img-slider-m img-slider">
+            {imgBanner?.map(image => (
+              <div className="banner__slide" key={image.img}>
+                <div className="banner__img img-slider__m">
+                  <div className="banner-wrapper ">
+                    <img alt={image.img} src={image.img} className="banner--image " />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </SlickSlider>
+
+
+
+          <TypeProduct />
+          <p className="title-product"> Tin mới đăng</p>
+          <div className="products container mobile-content my-3 my-sm-5">
+            <div className="d-flex flex-nowrap justify-content-between">
+              {/* <div className="products__sidebar pr-4 d-none d-sm-block">
             <div>Sản phẩm</div>
           </div> */}
-          <div className="flex-grow-1 w-100">
-            <div className="px-2 px-sm-0 mb-2"></div>
-            <div className=" products__cards mb-3 text-center pb-5 pt-5">
-              {products.products.map((product) => (
-                <Link href={`/products/${product.id}`}>
-                  <Card key={product.id}>
-
-                    <Typography component="h2">
-                      {product.name}
-                    </Typography><Typography component="h2">
-                      {product.price}
-                    </Typography>
-                  </Card>
-                </Link>
-              ))}
+              <div className="flex-grow-1 w-100">
+                <div className="px-2 px-sm-0 mb-2"></div>
+                <div className=" products__cards mb-3 text-center pb-5 pt-5">
+                  {products.products.map((product) => (
+                    <Link href={`/products/${product.id}`}>
+                      <Card key={product.id}>
+                        <img alt="/img/meme1.jpg" src="/img/meme1.jpg"/>
+                        <Typography component="h2" className="mt-1">
+                          {product.name}
+                        </Typography>
+                        <Typography component="h2">
+                          <div className="color-price-product mt-1">
+                            {product.price} đ
+                          </div>
+                          
+                        </Typography>
+                      </Card>
+                    </Link>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-        {/* {products.products.map((product) => (
-          <Link href={`/products/${product.id}`}>
-            <Card key={product.id} >
-            <div className="product-card-container">
-              
-                {product.name}
-              
-                {product.price}
-              </div>
-            </Card>
-          </Link>
-
-        ))} */}
         </div>
       </Container>
 
